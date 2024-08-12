6a6e445fd14c00decf1551f9a6e863f71e5239f4
BorisPenchev1 <b.penchev13@gmail.com>
2024-08-12 14:07:22 +0300

    Update loop.py
    Delete unnecesary code

--- Basic Syntax/loop.py
+++ Basic Syntax/loop.py
@@ -1,15 +1,4 @@
-# n = int(input())
 
-# for i in range (1, n + 1):
-#     num = int(input())
-#     if num % 2 != 0 :
-#         print(f"{num} is odd")
-#         break
-# else:
-#     print("All numbers are even")
-
-# Na gospodina
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 n = int(input())
 
